a513859326f5d31a17a6b9c14a11ffa23387e975
toriomara <budkevich.vladimir@gmail.com>
2020-12-13 17:35:43 +0400

    first commit

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import s from './Message.module.css'
 
-function Message() {
+
+interface MessageProps {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessageProps) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <img src={props.avatar} className={s.img} alt=""/>
+            <div className={s.text}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.textMessage}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
         </div>
     );
 }
